f786c5f66c9c1e29cb8698f1745b2af776d67d78
unknown <iskandar.django.work@gmail.com>
2026-05-23 12:47:36 +0500

    refactor: separate express app from http server initialization

---
+++ backend/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import { usersRouter } from "./src/users/usersRouter";
+import { roomsRouter } from "./src/rooms/roomsRouter";
+import { errorMiddleware } from "./src/error/errorMiddleware";
+
+export const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/users", usersRouter);
+app.use("/rooms", roomsRouter);
+app.use(errorMiddleware);
--- backend/server.ts
+++ backend/server.ts
@@ -1,36 +1,23 @@
-import express from "express";
 import { createServer } from "http";
-import { WebSocketServer } from "ws";
-import { database } from "./src/database/init";
-import { usersRouter } from "./src/users/usersRouter";
-import { errorMiddleware } from "./src/error/errorMiddleware";
-import cookieParser from "cookie-parser";
-import { roomsRouter } from "./src/rooms/roomsRouter";
+import { database } from "./src/models/database/init";
+import { app } from "./app";
 import { setupWs } from "./src/ws/wsSetup";
 
 const PORT: number = 3500;
 
-const app = express();
-
-app.use(express.json());
-app.use(cookieParser());
-app.use("/users", usersRouter);
-app.use("/rooms", roomsRouter);
-app.use(errorMiddleware);
-
 const server = createServer(app);
+
 setupWs(server);
 
 async function start() {
   try {
     await database.init();
     server.listen(PORT, () => {
-      console.log(`Server works on ${PORT} port`);
+      console.info(`➜  Local: http://localhost:${PORT}`);
+      console.info(`➜  press ^C to stop`);
     });
   } catch (e: unknown) {
-    console.log(
-      `Something went wrong.Error:${e instanceof Error ? e.message : e}`,
-    );
+    console.error(`Something went wrong. Error: ${e}`);
     process.exit(1);
   }
 }
