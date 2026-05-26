378a961dd3548d00e4f4729a79f96bbc2cf218c8
unknown <iskandar.django.work@gmail.com>
2026-05-26 14:03:55 +0500

    feat(register): create RegisterPage

--- frontend/src/pages/Registerpage.tsx
+++ frontend/src/pages/Registerpage.tsx
@@ -1,3 +1,47 @@
+import { useRef } from "react";
+import { createRegisterHandler } from "../handlers/RegisterHandler";
+
 export const Registerpage = () => {
-  return <h1>Registerpage</h1>;
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const registerHandler = createRegisterHandler({
+    usernameRef,
+    emailRef,
+    passwordRef,
+  });
+  return (
+    <>
+      <section className="register__section">
+        <form className="register__form" onSubmit={registerHandler}>
+          <input
+            className="form__input"
+            type="text"
+            placeholder="username"
+            max={8}
+            ref={usernameRef}
+            required
+          />
+          <input
+            className="form__input"
+            type="email"
+            placeholder="example@gmail.com"
+            ref={emailRef}
+            required
+          />
+          <input
+            className="form__input"
+            type="password"
+            placeholder="password"
+            min={8}
+            ref={passwordRef}
+            required
+          />
+
+          <button>Register</button>
+        </form>
+      </section>
+    </>
+  );
 };
