e98b606a5ed7aa3868e81e9b7598804df515aa5c
unknown <iskandar.django.work@gmail.com>
2026-05-26 14:35:36 +0500

    feat(login): create LoginPage component

--- frontend/src/pages/Loginpage.tsx
+++ frontend/src/pages/Loginpage.tsx
@@ -1,3 +1,41 @@
+import { useRef } from "react";
+import { Link } from "react-router";
+import { createLoginHandler } from "../handlers/loginHandler";
+import styles from "../style/register.module.css";
+
 export const Loginpage = () => {
-  return <h1>Loginpage</h1>;
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const loginHandler = createLoginHandler({ emailRef, passwordRef });
+  return (
+    <>
+      <section className={styles.register__section}>
+        <h1 className={styles.register__title}>Login</h1>
+        <form onSubmit={loginHandler} className={styles.register__form}>
+          <input
+            className={styles.form__input}
+            type="email"
+            placeholder="example@gmail.com"
+            ref={emailRef}
+            required
+          />
+          <input
+            className={styles.form__input}
+            type="password"
+            placeholder="password"
+            min={8}
+            ref={passwordRef}
+            required
+          />
+          <button className={styles.form__button}>Login</button>
+        </form>
+        <div>
+          <span>
+            Don't have an account? <Link to="/register">Register</Link>
+          </span>
+        </div>
+      </section>
+    </>
+  );
 };
