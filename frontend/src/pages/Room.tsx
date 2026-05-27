c5b4c11342624e5cc5ace04da78d426b24e9ab44
unknown <iskandar.django.work@gmail.com>
2026-05-27 12:28:07 +0500

    feat(rooms): create styles for RoomPage component

--- frontend/src/pages/Room.tsx
+++ frontend/src/pages/Room.tsx
@@ -1,31 +1,32 @@
 import { useRef } from "react";
 import { createRoomHandler } from "../handlers/roomHandlers";
+import styles from "../style/rooms.module.css";
 
 export const Room = () => {
   const roomIdRef = useRef<HTMLInputElement>(null);
   const roomHandler = createRoomHandler(roomIdRef);
   return (
     <>
-      <section className="room">
-        <div className="create__room">
-          <h1 className="room__title">Create your room</h1>
-          <ul className="room__list">
-            <li className="list__item">Limit time: 1 hour</li>
-            <li className="list__item">Unlimited friends</li>
+      <section className={styles.room}>
+        <div className={styles.create__room}>
+          <h1 className={styles.room__title}>Create your room</h1>
+          <ul className={styles.room__list}>
+            <li className={styles.list__item}>Limit time: 1 hour</li>
+            <li className={styles.list__item}>Unlimited friends</li>
           </ul>
-          <button className="room__button" onClick={roomHandler}>
+          <button className={styles.room__button} onClick={roomHandler}>
             Create room
           </button>
         </div>
-        <div className="join__room">
-          <h1 className="room__title">Join room</h1>
+        <div className={styles.join__room}>
+          <h1 className={styles.room__title}>Join room</h1>
           <input
             type="text"
             placeholder="Enter room id"
-            className="room__input"
+            className={styles.room__input}
             ref={roomIdRef}
           />
-          <button className="room__button">Join room</button>
+          <button className={styles.room__button}>Join room</button>
         </div>
       </section>
     </>
